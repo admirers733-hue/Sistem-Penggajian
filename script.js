b3457443dd80a47c36f8beb64fa0740d8cec9b84
Nama Mahasiswa <email@gmail.com>
2026-07-15 16:03:48 +0700

    Menambahkan fitur login dan CRUD

---
+++ script.js
@@ -0,0 +1,111 @@
+function login(){
+
+var user=document.getElementById("username").value;
+
+var pass=document.getElementById("password").value;
+
+if(user=="admin" && pass=="123"){
+
+window.location="dashboard.html";
+
+}else{
+
+alert("Login Gagal");
+
+}
+
+}
+
+var data=[];
+
+function tambahData(){
+
+var nama=document.getElementById("nama").value;
+
+var jabatan=document.getElementById("jabatan").value;
+
+var gaji=document.getElementById("gaji").value;
+
+data.push({
+
+nama:nama,
+
+jabatan:jabatan,
+
+gaji:gaji
+
+});
+
+tampil();
+
+}
+
+function tampil(){
+
+var tabel=document.getElementById("tabel");
+
+tabel.innerHTML="";
+
+for(var i=0;i<data.length;i++){
+
+tabel.innerHTML+=
+
+"<tr>"+
+
+"<td>"+(i+1)+"</td>"+
+
+"<td>"+data[i].nama+"</td>"+
+
+"<td>"+data[i].jabatan+"</td>"+
+
+"<td>"+data[i].gaji+"</td>"+
+
+"<td><button onclick='hapus("+i+")'>Hapus</button></td>"+
+
+"</tr>";
+
+}
+
+}
+
+function hapus(i){
+
+data.splice(i,1);
+
+tampil();
+
+}
+
+function cariData(){
+
+var cari=document.getElementById("cari").value.toLowerCase();
+
+var tabel=document.getElementById("tabel");
+
+tabel.innerHTML="";
+
+for(var i=0;i<data.length;i++){
+
+if(data[i].nama.toLowerCase().includes(cari)){
+
+tabel.innerHTML+=
+
+"<tr>"+
+
+"<td>"+(i+1)+"</td>"+
+
+"<td>"+data[i].nama+"</td>"+
+
+"<td>"+data[i].jabatan+"</td>"+
+
+"<td>"+data[i].gaji+"</td>"+
+
+"<td><button onclick='hapus("+i+")'>Hapus</button></td>"+
+
+"</tr>";
+
+}
+
+}
+
+}
