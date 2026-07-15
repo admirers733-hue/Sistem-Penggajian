68d866d478adb6bbf08eedfe18ed2292c26cc13d
Nama Mahasiswa <email@gmail.com>
2026-07-15 17:28:24 +0700

    Menambahkan fitur login

--- script.js
+++ script.js
@@ -1,12 +1,145 @@
-function login() {
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    font-family:Arial, Helvetica, sans-serif;
+}
 
-    var user = document.getElementById("username").value;
-    var pass = document.getElementById("password").value;
+body{
+    background:#eaf3ff;
+}
 
-    if (user === "RESINNA" && pass === "24110023") {
-        alert("Login Berhasil");
-        window.location.href = "dashboard.html";
-    } else {
-        alert("Login Gagal");
-    }
+/* LOGIN */
+
+.login-box{
+    width:400px;
+    background:white;
+    margin:80px auto;
+    padding:30px;
+    border-radius:12px;
+    box-shadow:0 0 15px rgba(0,0,0,0.2);
+}
+
+.login-box h1{
+    text-align:center;
+    color:#1565c0;
+    margin-bottom:10px;
+}
+
+.login-box p{
+    text-align:center;
+    margin-bottom:20px;
+}
+
+.login-box input{
+    width:100%;
+    padding:12px;
+    margin-bottom:15px;
+    border:1px solid #ccc;
+    border-radius:8px;
+}
+
+.login-box button{
+    width:100%;
+    padding:12px;
+    border:none;
+    background:#1565c0;
+    color:white;
+    border-radius:8px;
+    cursor:pointer;
+    font-size:16px;
+}
+
+.login-box button:hover{
+    background:#0d47a1;
+}
+
+.info{
+    margin-top:20px;
+    background:#f5f5f5;
+    padding:10px;
+    border-radius:8px;
+}
+
+/* DASHBOARD */
+
+.container{
+    width:90%;
+    margin:30px auto;
+}
+
+.container h1{
+    text-align:center;
+    color:#1565c0;
+    margin-bottom:20px;
+}
+
+.card{
+    background:white;
+    padding:20px;
+    border-radius:10px;
+    margin-bottom:20px;
+    box-shadow:0 0 10px rgba(0,0,0,0.1);
+}
+
+.card h2{
+    margin-bottom:15px;
+    color:#1565c0;
+}
+
+label{
+    display:block;
+    margin-top:10px;
+}
+
+input{
+    width:100%;
+    padding:10px;
+    margin-top:5px;
+    margin-bottom:10px;
+    border:1px solid #ccc;
+    border-radius:6px;
+}
+
+.button-group button{
+    padding:10px 18px;
+    margin-right:10px;
+    border:none;
+    border-radius:6px;
+    background:#1565c0;
+    color:white;
+    cursor:pointer;
+}
+
+.button-group button:hover{
+    background:#0d47a1;
+}
+
+table{
+    width:100%;
+    border-collapse:collapse;
+}
+
+table th{
+    background:#1565c0;
+    color:white;
+    padding:12px;
+}
+
+table td{
+    padding:10px;
+    text-align:center;
+    border-bottom:1px solid #ddd;
+}
+
+table tr:hover{
+    background:#f1f7ff;
+}
+
+button.edit{
+    background:green;
+}
+
+button.hapus{
+    background:red;
 }
